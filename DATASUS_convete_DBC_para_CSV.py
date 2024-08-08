a891becea273a0eda694dc82995c4ae13f79f60b
Brenno - BCL-ST <dev@bcl-st.com.br>
2024-08-07 22:39:53 -0300

    feat: implementing environmental variables

--- DATASUS_convete_DBC_para_CSV.py
+++ DATASUS_convete_DBC_para_CSV.py
@@ -1,10 +1,20 @@
 import readdbc
 
+import os
+from dotenv import load_dotenv
+
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
+
+# Acesse as variáveis de ambiente
+dbc_file = os.getenv("DBC_FILE")
+csv_file = os.getenv("CSV_FILE")
+
 # Caminho do arquivo DBC
-dbc_file = './DBC/PAPE2405.dbc'
+# dbc_file = './DBC/PAPE2405.dbc'
 
 # Caminho do arquivo CSV de saída
-csv_file = './CSV/PA_Pernambuco_2024_05.csv'
+# csv_file = './CSV/PA_Pernambuco_2024_05.csv'
 
 # Converter DBC para CSV
 readdbc.dbc2csv(dbc_file, csv_file)
